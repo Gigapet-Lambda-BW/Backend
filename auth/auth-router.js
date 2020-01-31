b10ae7db97d817ae385532ce1f8b8f37d735e7ff
Michael Williams <manicdesigner@gmail.com>
2020-01-30 20:24:49 -0500

    module export auth-router

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -23,3 +23,37 @@ router.post('/register', async (req, res) => {
     res.status(500).json({ errorMessage: 'server error registering user' });
   }
 });
+
+router.post('/login', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const user = await authModel.findBy({ username });
+
+    const passwordCheck = await bcrypt.compare(password, user.password);
+
+    if (user && passwordCheck) {
+      const token = jwt.sign(
+        {
+          subject: user.id,
+          username: user.username,
+        },
+        secrets.jwt,
+        {
+          expiresIn: '5d',
+        }
+      );
+
+      res
+        .status(200)
+        .json({ token: token, message: `greetings ${user.username}` });
+    } else {
+      res.status(401).json({ message: 'invalid user credentials' });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: 'login server error' });
+  }
+});
+
+module.exports = router;
