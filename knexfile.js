c182368266d10b1ae2635a1427142d3497fadf0b
Michael Williams <manicdesigner@gmail.com>
2020-01-31 21:50:49 -0500

    update migrations and dbs

--- knexfile.js
+++ knexfile.js
@@ -39,14 +39,4 @@ module.exports = {
       },
     },
   },
-  production: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL,
-    migrations: {
-      directory: './data/migrations',
-    },
-    seeds: {
-      directory: './data/seeds',
-    },
-  },
 };
