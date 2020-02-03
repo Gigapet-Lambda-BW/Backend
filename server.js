dc00acb33e0399d5c6297efc31a797b8d6a5f855
Michael Williams <manicdesigner@gmail.com>
2020-02-03 11:47:30 -0500

    add 404 and 500 error with next

--- server.js
+++ server.js
@@ -15,4 +15,13 @@ server.get('/', (req, res) => {
   res.send('Gigapet 7 is alive!');
 });
 
+server.use((req, res) => {
+  res.status(404).json({ message: '404 route was not found' });
+});
+
+server.use((err, req, res, next) => {
+  console.log(err); // ! rm when complete
+  res.status(500).json({ message: 'an internal server error occurred' });
+});
+
 module.exports = server;
