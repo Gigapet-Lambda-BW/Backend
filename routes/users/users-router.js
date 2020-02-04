6138b7380abd6ba5ff9f486473e0a90d78f4d4ca
Michael Williams <manicdesigner@gmail.com>
2020-02-04 10:47:36 -0500

    add update username and password

--- routes/users/users-router.js
+++ routes/users/users-router.js
@@ -13,7 +13,28 @@ router.get('/:id', async (req, res, next) => {
       res.status(404).json({ message: 'no user by id' });
     }
   } catch (err) {
-    console.log(err); // ! rm for production
+    next(err);
+  }
+});
+
+router.put('/:id', async (req, res, next) => {
+  const { username, password } = req.body;
+  const { id } = req.params;
+  if (!username) {
+    return res.status(400).json({ message: 'username required for update' });
+  }
+  if (!password) {
+    return res.status(400).json({ message: 'password required for update' });
+  }
+  const updateUser = {
+    username: username,
+    password: password,
+  };
+  try {
+    const data = await userModel.updateUser(updateUser, id);
+    res.status(200).json(data);
+  } catch (err) {
+    console.log(err);
     next(err);
   }
 });
